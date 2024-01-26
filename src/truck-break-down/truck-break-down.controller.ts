a913db93a56da273cd9f5182f3df3fb15bdc3f6c
JamalSanei <jamal.sanei@gmail.com>
2024-01-26 10:50:50 +0330

    add: get truckbreakdown by driverId

--- src/truck-break-down/truck-break-down.controller.ts
+++ src/truck-break-down/truck-break-down.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { TruckBreakDownService } from './truck-break-down.service';
 
@@ -9,6 +9,20 @@ export class TruckBreakDownController {
   async getAll(@Res() response: Response) {
     try {
       const res = await this.truckBreakDownService.getAll();
+      response.status(res.status).json(res.data);
+    } catch (err) {
+      console.log(err);
+      response.status(500).json(err);
+    }
+  }
+
+  @Get('/api/truckbreakdown/:driverId')
+  async getByUserId(
+    @Param('driverId') driverId: number,
+    @Res() response: Response,
+  ) {
+    try {
+      const res = await this.truckBreakDownService.getByUserId(driverId);
       response.status(res.status).json(res.data);
     } catch (err) {
       console.log(err);
--- src/truck-break-down/truck-break-down.service.ts
+++ src/truck-break-down/truck-break-down.service.ts
@@ -35,6 +35,34 @@ export class TruckBreakDownService {
     };
   }
 
+  async getByUserId(driverId: number) {
+    let data = [];
+    const res = await this.truckBreakDownRepository.findAndCountAll({
+      where: {
+        driverId: driverId,
+      },
+    });
+
+    for (let item of res.rows) {
+      let breakDown = {};
+      const answers = await this.truckBreakDownItemsRepository.findOne({
+        where: {
+          id: item.truckBreakDownItemsId,
+        },
+      });
+      //   console.log(answers);
+      breakDown = item.dataValues;
+      breakDown['answers'] = answers.dataValues;
+      data.push(breakDown);
+      //   console.log(data);
+    }
+    return {
+      status: 200,
+      data: data,
+      count: res.count,
+    };
+  }
+
   //   async update(breakDownid: number){
   //     const res = await this.tr
 

06d8e6e093255f837daa9914c10b6d7435a6c90a
Jamal.Sanei <jamal.sanei@gmail.com>
2024-10-20 00:21:29 +0330

    modified; calculate statistic for last seven days

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { HttpStatusCode } from 'axios';
 import { CheckListService } from './check-list/check-list.service';
 import { OrderService } from './order/services/order.service';
 import { TruckBreakDownService } from './truck-break-down/truck-break-down.service';
-import { statistics } from './static/constant';
+import { CalculateStatistics, Statistics, statistics } from './static/constant';
 
 @Injectable()
 export class AppService {
@@ -13,71 +13,83 @@ export class AppService {
     private readonly truckBreakDownService: TruckBreakDownService,
   ) {}
   //PND: cron implement for end of day test
-  async calculateStatistics(day: string, dateFront: string) {
-    try {
-      // const today = this.orderService.getTime().message.result; // formate sample date: 2022/1/23
-      //CHK: result test
 
+  async calculateStatisticsOnDateCustomFormat(
+    dateCustomFormat: string,
+  ): Promise<CalculateStatistics> {
+    try {
       const checkListStatistic = await this.checklistService.dailyCheckCount(
-        day,
+        dateCustomFormat,
         undefined,
         undefined,
       ); // return both "register" and "unregistered" checkList
-      //CHK: result
+
       const necessaryTruckBreakDownDaily =
         await this.truckBreakDownService.transportUserGetAll(
           'false',
           'false',
           'true',
-          day,
-          day,
+          dateCustomFormat,
+          dateCustomFormat,
         );
 
-      //CHK: result
       const inprogressTruckBreakDownDaily =
         await this.truckBreakDownService.transportUserGetAll(
           'true',
           'false',
           'true',
-          day,
-          day,
+          dateCustomFormat,
+          dateCustomFormat,
         );
-      //CHK: result
+
       const doneTruckBreakDownDaily =
         await this.truckBreakDownService.transportUserGetAll(
           'false',
           'true',
           'true',
-          day,
-          day,
+          dateCustomFormat,
+          dateCustomFormat,
         );
+      return {
+        checklist: checkListStatistic.data,
+        activitiesInProgress: inprogressTruckBreakDownDaily.data as number,
+        activitiesPerformed: doneTruckBreakDownDaily.data as number,
+        requiredActivities: necessaryTruckBreakDownDaily.data as number,
+      };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async insertStatistics(
+    dateIsoFormat: string,
+    calculateStatistics: CalculateStatistics,
+  ) {
+    try {
+      // const today = this.orderService.getTime().message.result; // formate sample date: 2022/1/23
 
       statistics.checklistRegistered.push({
-        date: dateFront,
-        statistics: checkListStatistic.data['countRegister'],
+        date: dateIsoFormat,
+        statistics: calculateStatistics.checklist['countRegister'],
       });
       statistics.checklistUnRegistered.push({
-        date: dateFront,
-        statistics: checkListStatistic.data['countUnRegister'],
+        date: dateIsoFormat,
+        statistics: calculateStatistics.checklist['countUnRegister'],
       });
       statistics.requiredActivities.push({
-        date: dateFront,
-        statistics: necessaryTruckBreakDownDaily.data as number,
+        date: dateIsoFormat,
+        statistics: calculateStatistics.requiredActivities,
       });
       statistics.activitiesInProgress.push({
-        date: dateFront,
-        statistics: inprogressTruckBreakDownDaily.data as number,
+        date: dateIsoFormat,
+        statistics: calculateStatistics.activitiesInProgress,
       });
       statistics.activitiesPerformed.push({
-        date: dateFront,
-        statistics: doneTruckBreakDownDaily.data as number,
+        date: dateIsoFormat,
+        statistics: calculateStatistics.activitiesPerformed,
       });
-      // return {
-      //   checklist: checkListStatistic.data,
-      //   truckbreakdownNecessary: necessaryTruckBreakDownDaily.data,
-      //   truckbreakdownInprogress: inprogressTruckBreakDownDaily.data,
-      //   truckbreakdownDone: doneTruckBreakDownDaily.data,
-      // };
+
+      statistics.lastDate = new Date();
+
       // PND: update result of calculates in "memcache"
     } catch (err) {
       console.log(err);
@@ -85,17 +97,93 @@ export class AppService {
     }
   }
 
+  async updateLatestStatistics(
+    dateIsoFormat: string,
+    calculateStatistics: CalculateStatistics,
+  ) {
+    try {
+      statistics.checklistRegistered[6] = {
+        date: dateIsoFormat,
+        statistics: calculateStatistics.checklist['countRegister'],
+      };
+      statistics.checklistUnRegistered[6] = {
+        date: dateIsoFormat,
+        statistics: calculateStatistics.checklist['countUnRegister'],
+      };
+      statistics.requiredActivities[6] = {
+        date: dateIsoFormat,
+        statistics: calculateStatistics.requiredActivities,
+      };
+      statistics.activitiesInProgress[6] = {
+        date: dateIsoFormat,
+        statistics: calculateStatistics.activitiesInProgress,
+      };
+      statistics.activitiesPerformed[6] = {
+        date: dateIsoFormat,
+        statistics: calculateStatistics.activitiesPerformed,
+      };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  shiftfirstestStatistics() {
+    try {
+      //CHK: is correct this ?
+      // Object.keys(statistics).forEach((item) => {
+      //   statistics[item].shift();
+      // });
+      statistics.checklistRegistered.shift();
+      statistics.checklistUnRegistered.shift();
+      statistics.requiredActivities.shift();
+      statistics.activitiesInProgress.shift();
+      statistics.activitiesPerformed.shift();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   async calculateSevenLastDayStatistics() {
-    // const lastSevenDaysDate: string[] = [];
-    const today = new Date();
     let customDay = new Date();
-    if (!statistics.checklistRegistered.length) {
+    const tody = new Date();
+    if (statistics.checklistRegistered.length) {
+      if (tody.getDay() === statistics.lastDate.getDay()) {
+        console.log('generate update last day');
+        const dateCustomFormat =
+          this.orderService.getTime(customDay).message.result;
+        const calculateStatistics =
+          await this.calculateStatisticsOnDateCustomFormat(dateCustomFormat);
+        await this.updateLatestStatistics(
+          customDay.toISOString(),
+          calculateStatistics,
+        );
+      } else {
+        console.log('generate shift date');
+        const dateCustomFormat =
+          this.orderService.getTime(customDay).message.result;
+        const calculateStatistics =
+          await this.calculateStatisticsOnDateCustomFormat(dateCustomFormat);
+
+        this.shiftfirstestStatistics();
+        await this.insertStatistics(
+          customDay.toISOString(),
+          calculateStatistics,
+        );
+        //pop first index and push new statistics
+      }
+    } else {
+      console.log('generate last 7 days');
       for (let dayOffset = 6; dayOffset > -1; dayOffset--) {
-        customDay.setDate(today.getDate() - dayOffset);
+        customDay.setDate(tody.getDate() - dayOffset);
         // console.log('generate custom day: ', customDay);
-        const date = this.orderService.getTime(customDay).message.result;
-        await this.calculateStatistics(date, customDay.toISOString());
-        // lastSevenDaysDate.push(date);
+        const dateCustomFormat =
+          this.orderService.getTime(customDay).message.result;
+        const calculateStatistics =
+          await this.calculateStatisticsOnDateCustomFormat(dateCustomFormat);
+        await this.insertStatistics(
+          customDay.toISOString(),
+          calculateStatistics,
+        );
       }
     }
 
--- src/static/constant.ts
+++ src/static/constant.ts
@@ -3,7 +3,15 @@ interface DateStatistic {
   statistics?: number;
 }
 
+export interface CalculateStatistics {
+  checklist: Object;
+  requiredActivities: number;
+  activitiesInProgress: number;
+  activitiesPerformed: number;
+}
+
 export interface Statistics {
+  lastDate?: Date;
   checklistRegistered: Array<DateStatistic>;
   checklistUnRegistered: Array<DateStatistic>;
   requiredActivities: Array<DateStatistic>;

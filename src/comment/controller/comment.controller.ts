4592e268239c5ef250edcc549cb5375db19a7208
peymanslayer <peymantaghitash2024@gmail.com>
2025-01-16 23:41:33 +0330

    fix bug insert checklist

--- src/comment/controller/comment.controller.ts
+++ src/comment/controller/comment.controller.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Controller, Get,Body,Res ,Post, Delete } from '@nestjs/common';
 import { CommentService } from '../services/comment.service';
 import { InsertCommentDto } from '../dto/insert.comment.dto';
@@ -15,26 +15,4 @@ export class CommentController{
   }catch(err){
     response.status(500).json('internal server errror')
   }
-}
-
-=======
-import { Controller, Get,Body,Res ,Post, Delete } from '@nestjs/common';
-import { CommentService } from '../services/comment.service';
-import { InsertCommentDto } from '../dto/insert.comment.dto';
-import { Response } from 'express';
-
-@Controller()
-export class CommentController{
-  constructor(private readonly commentService:CommentService) {}
-  @Post('/api/insertComment')
-  async insertComment(@Body() body:InsertCommentDto,@Res() response:Response ){
-  try{
-   const insertComment=await this.commentService.insertComment(body.comment,body.shopId);
-   response.status(insertComment.status).json(insertComment.message)
-  }catch(err){
-    response.status(500).json('internal server errror')
-  }
-}
-
->>>>>>> 06d6f4c440da2b182c6703835946f16d97950290
-}
+  }}
